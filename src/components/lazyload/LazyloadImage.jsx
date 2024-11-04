a8555edda586c2151c3f58c3f0d5dea97870b3bb
RajabAabed <rajab7abed@gmail.com>
2024-11-04 13:41:34 +0200

    placeholder

--- src/components/lazyload/LazyloadImage.jsx
+++ src/components/lazyload/LazyloadImage.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable react/prop-types */
 import LazyLoad from "react-lazyload";
-
+import placeholder from "../../assets/images/placeholder.webp";
 export default function LazyloadIamge({ imageScr, imageAlt, classes }) {
   return (
     <LazyLoad
       height={200}
       offset={100}
-      placeholder={<div></div>}
+      placeholder={
+        <img
+          src={placeholder}
+          alt="placeholder"
+          className={classes}
+        />
+      }
     >
       <img
         src={imageScr}
